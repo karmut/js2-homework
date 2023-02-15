3023594259a97606b06ba60d119a9ea5539316eb
karmut <karina.mutafyan@gmail.com>
2023-02-15 10:10:30 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let n1 = prompt();
+let n2 = prompt();
+
+let number1 = Number(n1); // string->number
+let number2 = Number(n2); // string->number
+
+alert(number1 + number2);
+
+let resultat = number1 + number2;
+
+if (resultat > 10) {
+  alert("Okay!");
+} else {
+  alert("Error!");
+}
+
+//----------------------------------------------------------
+
+let word = prompt();
+
+if ((word = karina)) {
+  alert("Hello!");
+} else {
+  alert("who are you?!");
+}
